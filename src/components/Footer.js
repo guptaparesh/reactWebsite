9a52fba5cd6988ee1dc7472b211c661cd6deca3a
Paresh Gupta <guptaparesh@gmail.com>
2022-09-29 11:04:28 -0700

    linked in button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     return (
         <BottomNavigation >
           <BottomNavigationAction color="red" label="Github" value="Paresh's git" icon={<GitHubIcon  style={{fill: "#3b5998"}}/>} href="https://github.com/guptaparesh" target="_blank" />
-          <BottomNavigationAction color="blue" label="LinkedInProfile" value="Paresh's LinkedIn" icon={<LinkedInIcon  style={{fill: "#3b5998"}}/>} href="https://www.linkedin.com/in/paresh-gupta-0420522/" target="_blank" />
+          <BottomNavigationAction color="blue" label="LinkedInProfile" value="Paresh's LinkedIn" icon={<LinkedInIcon  style={{fill: "#3b5998"}}/>} href="www.linkedin.com/in/paresh-gupta" target="_blank" />
         </BottomNavigation>
         
 )
